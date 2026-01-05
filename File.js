6bf0c1af8db31b24447327460ae1be6585eefcb2
Mohitha-n220321 <n220321@rguktn.ac.in>
2026-01-05 17:01:14 +0000

    updated the website with other webpage

---
+++ File.js
@@ -0,0 +1,18 @@
+
+    function getWeather(){
+        var city = document.getElementById("cityInput").value;
+        var date = document.getElementById("dateInput").value;
+        var temp = "28°C";
+        var weather = "Sunny";
+
+        document.getElementById("cityName").innerText = city;
+        document.getElementById("temperature").innerText = temp;
+        document.getElementById("condition").innerText = weather;
+        document.getElementById("dateDisplay").innerText = date;
+    }
+    function goToDemo() {
+    window.location.href = "demo.html";
+}
+
+
+
